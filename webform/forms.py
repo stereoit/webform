c547a2f39ce46f0a187cbd8ca9e46ebbf6af30e1
robert <smol.robert@gmail.com>
2017-10-19 00:04:14 +0200

    Added support for translation

--- webform/forms.py
+++ webform/forms.py
@@ -1,26 +1,35 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 class ApplicantForm(forms.Form):
     your_name = forms.CharField(
-        label='Your name',
+        label=_('your name'),
+        help_text=_('applicantform.your_name.help_text'),
         max_length=100,
         required=True
     )
     test_name = forms.CharField(
-        label='Test field',
+        label=_('test field'),
         max_length=100,
         # help_text="`joe` => `boby`, `dan` == invalid ValidationError, anything else is just passed through"
-        help_text="Value = `dan` => invalid ValidationError, anything else is just passed through"
+        help_text=_("Value = `dan` => invalid ValidationError, anything else is just passed through"),
     )
     start_date = forms.DateField(
+        label=_("possible join date"),
+        # Translators: This message appears on the home page only
         help_text="Enter a date between now and 4 weeks.",
         required=False,
         widget=forms.widgets.DateInput(attrs={'type': 'date'})
     )
-    message = forms.CharField(widget=forms.Textarea, required=False)
-    tickbox = forms.BooleanField(
+    message = forms.CharField(
+        label=_('message'),
+        widget=forms.Textarea,
         required=False,
-        help_text="Just tick it"
+    )
+    tickbox = forms.BooleanField(
+        label=_('tickbox'),
+        required=False,
+        help_text=_("This is a sample tick box."),
     )
 
     def clean_test_name(self):
